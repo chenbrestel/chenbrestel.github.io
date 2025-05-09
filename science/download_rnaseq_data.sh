860b2fb23e12b03fbfcf69225adffdc6ee3436a9
Chen Brestel <chen.brestel@gmail.com>
2025-05-09 10:12:32 +0300

    ohel_bilhah.png

---
+++ science/download_rnaseq_data.sh
@@ -0,0 +1,25 @@
+#
+# Dataset
+#https://www.ncbi.nlm.nih.gov/bioproject/790815
+
+# SRA Data selector
+#https://www.ncbi.nlm.nih.gov/Traces/study/?
+
+#Run	SRR17278829
+
+# Download
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR172/029/SRR17278829/SRR17278829_1.fastq.gz  # ENA
+
+#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR172/029/SRR17278829/SRR17278829.fastq.gz
+
+#wget -r ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR172/029/SRR17278829
+
+#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR172/029/SRR17278829/SRR17278829.fastq.gz
+
+#wget -r ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR172/029/SRR17278829/SRR17278829_*.fastq.gz
+
+#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR172/788/SRR17278829/SRR17278829_1.fastq.gz
+#wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR172/008/SRR17278829/SRR17278829_1.fastq.gz
+
+#wget "ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR101/006/SRR1016916/*"
+#wget "ftp://ftp.sra.ebi.ac.uk/vol1/fastq//SRR17278829/*"
